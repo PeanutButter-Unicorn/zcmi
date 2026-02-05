7f449d828e19eaecbc1f55dac3793f7dc1cf0419
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2026-02-04 22:39:56 -0700

    refactor: remove unused methods

--- src/main/kotlin/lol/pbu/BaseCommand.kt
+++ src/main/kotlin/lol/pbu/BaseCommand.kt
@@ -1,7 +1,6 @@
 package lol.pbu
 
 import io.micronaut.core.annotation.ReflectiveAccess
-import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Help.Ansi.AUTO
 import picocli.CommandLine.Model.CommandSpec
@@ -21,18 +20,6 @@ open class BaseCommand {
 
     fun err(message: String?) {
         errWriter()?.println(AUTO.string("@|bold,red | Error|@ $message"))
-    }
-
-    fun warning(message: String?) {
-        outWriter()?.println(AUTO.string("@|bold,red | Warning|@ $message"))
-    }
-
-    fun green(message: String?) {
-        outWriter()?.println(AUTO.string("@|bold,green $message|@"))
-    }
-
-    fun red(message: String?) {
-        outWriter()?.println(AUTO.string("@|bold,red $message|@"))
     }
 
     fun outWriter(): PrintWriter? = spec?.commandLine()?.getOut()
